91ecbfbbf6ea9044161590d8a2625265d6c4103d
Derek Vasconcelos <derek@cepetro.unicamp.br>
2021-12-08 20:15:28 -0300

    New nvim-autopairs config

--- nvim/.config/nvim/after/plugin/nvim-autopairs.lua
+++ nvim/.config/nvim/after/plugin/nvim-autopairs.lua
@@ -1,40 +1,15 @@
-local remap = vim.api.nvim_set_keymap
 local npairs = require('nvim-autopairs')
+local cmp = require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
--- Mapping <CR> with nvim-compe
-_G.MUtils= {}
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-    else
-      return npairs.esc("<cr>")
-    end
-  else
-    return npairs.autopairs_cr()
-  end
-end
-remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
--- Treesitter integration
 npairs.setup({
-    check_ts = true,
-    ts_config = {
-        lua = {'string'},-- it will not add pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
-})
-require('nvim-treesitter.configs').setup {
-    autopairs = {enable = true}
-}
-local ts_conds = require('nvim-autopairs.ts-conds')
-local Rule = require('nvim-autopairs.rule')
--- press % => %% is only inside comment or string
-npairs.add_rules({
-  Rule("%", "%", "lua")
-    :with_pair(ts_conds.is_ts_node({'string','comment'})),
-  Rule("$", "$", "lua")
-    :with_pair(ts_conds.is_not_ts_node({'function'}))
+	check_ts = true,
+	ts_config = {
+		lua = { "string", "source" },
+		javascript = { "string", "template_string" },
+		java = false,
+	},
+	enable_check_bracket_line = false,
 })
