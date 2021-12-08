8689fa5d7dad130b42247a19ec81b67af6c021db
Derek Vasconcelos <derek@cepetro.unicamp.br>
2021-12-08 20:16:19 -0300

    Changed to nvim-cmp completion

--- nvim/.config/nvim/lua/dbvasconcelos/plugins.lua
+++ nvim/.config/nvim/lua/dbvasconcelos/plugins.lua
@@ -1,12 +1,15 @@
 -- Bootstrap
-local install_path = vim.fn.stdpath("data") ..
-                         "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({
-        "git", "clone", "--depth", "1",
-        "https://github.com/wbthomason/packer.nvim", install_path,
-    })
-    vim.cmd "packadd packer.nvim"
+	vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	vim.cmd("packadd packer.nvim")
 end
 
 -- Auto sync plugins when editing this file
@@ -19,137 +22,159 @@ vim.cmd([[
 
 -- Declarations
 return require("packer").startup({
-    function(use)
+	function(use)
+		-- Plugin Manager itself
+		use("wbthomason/packer.nvim")
 
-        -- Plugin Manager itself
-        use "wbthomason/packer.nvim"
+		-- Color Scheme
+		use("eddyekofo94/gruvbox-flat.nvim")
 
-        -- Color Scheme
-        use "eddyekofo94/gruvbox-flat.nvim"
+		-- Status Line
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		})
 
-        -- Status Line
-        use {
-            "nvim-lualine/lualine.nvim",
-            requires = {'kyazdani42/nvim-web-devicons', opt = true},
-        }
+		-- File Tree
+		use({
+			"kyazdani42/nvim-tree.lua",
+			requires = "kyazdani42/nvim-web-devicons",
+		})
 
-        -- File Tree
-        use {
-            "kyazdani42/nvim-tree.lua",
-            requires = "kyazdani42/nvim-web-devicons",
-        }
+		-- Terminal
+		use("akinsho/nvim-toggleterm.lua")
 
-        -- Terminal
-        use "akinsho/nvim-toggleterm.lua"
+		-- Clipboard
+		use("AckslD/nvim-neoclip.lua")
 
-        -- Clipboard
-        use "AckslD/nvim-neoclip.lua"
+		-- LSP Support
+		use("neovim/nvim-lspconfig")
 
-        -- LSP Support
-        use "neovim/nvim-lspconfig"
+		-- LSP Formatting
+		use("mhartington/formatter.nvim")
 
-        -- LSP Formatting
-        use "mhartington/formatter.nvim"
+		-- LSP Installation
+		use("williamboman/nvim-lsp-installer")
 
-        -- LSP Installation
-        use "kabouzeid/nvim-lspinstall"
+		-- Commenting
+		use("numToStr/Comment.nvim")
 
-        -- Commenting
-        use "terrortylor/nvim-comment"
+		-- Auto Completion
+		use({
+			"hrsh7th/nvim-cmp",
+			requires = {
+				"L3MON4D3/LuaSnip",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-path",
+				"saadparwaiz1/cmp_luasnip",
+			},
+		})
 
-        -- Auto Completion
-        use {
-            "hrsh7th/nvim-cmp",
-            requires = {
-                "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip",
-                "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-path", "hrsh7th/cmp-nvim-lua",
-            },
-        }
+		-- Debugging
+		use({
+			"mfussenegger/nvim-dap",
+			requires = "theHamsta/nvim-dap-virtual-text",
+		})
 
-        -- Debugging
-        use {
-            'mfussenegger/nvim-dap',
-            requires = 'theHamsta/nvim-dap-virtual-text',
-        }
+		-- Refactoring
+		use({
+			"ThePrimeagen/refactoring.nvim",
+			requires = {
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-treesitter/nvim-treesitter" },
+			},
+		})
 
-        -- Refactoring
-        use {
-            "ThePrimeagen/refactoring.nvim",
-            requires = {
-                {"nvim-lua/plenary.nvim"}, {"nvim-treesitter/nvim-treesitter"},
-            },
-        }
+		-- Treesitter
+		use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-        -- Treesitter
-        use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+		-- GoLang Utilities
+		use({ "fatih/vim-go", run = ":GoUpdateBinaries" })
 
-        -- GoLang Utilities
-        use {'fatih/vim-go', run = ':GoUpdateBinaries'}
+		-- Auto Pairing Parenthesis/Brackets/Curly
+		use("windwp/nvim-autopairs")
 
-        -- Auto Pairing Parenthesis/Brackets/Curly
-        use 'windwp/nvim-autopairs'
+		-- Fuzzy Finder
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				{ "nvim-lua/popup.nvim" },
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-telescope/telescope-fzy-native.nvim" },
+				{ "nvim-telescope/telescope-dap.nvim" },
+			},
+		})
 
-        -- Fuzzy Finder
-        use {
-            'nvim-telescope/telescope.nvim',
-            requires = {
-                {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'},
-                {'nvim-telescope/telescope-fzy-native.nvim'},
-                {'nvim-telescope/telescope-dap.nvim'},
-            },
-        }
+		-- Color Code Highlighting
+		use("norcalli/nvim-colorizer.lua")
 
-        -- Color Code Highlighting
-        use 'norcalli/nvim-colorizer.lua'
+		-- Undo Tree
+		use("mbbill/undotree")
 
-        -- Undo Tree
-        use 'mbbill/undotree'
+		-- Hotkey Helper
+		use("folke/which-key.nvim")
 
-        -- Hotkey Helper
-        use 'folke/which-key.nvim'
+		-- Surround text objects
+		use("tpope/vim-surround")
 
-        -- Surround text objects
-        use 'tpope/vim-surround'
+		-- Git Integration
+		use("tpope/vim-fugitive")
 
-        -- Git Integration
-        use 'tpope/vim-fugitive'
+		-- Repeat command (.) for plugins
+		use("tpope/vim-repeat")
 
-        -- Repeat command (.) for plugins
-        use 'tpope/vim-repeat'
+		-- Git Signs
+		use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
 
-        -- Git Signs
-        use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
+		-- Extend block navigation to language keywords
+		use("andymass/vim-matchup")
 
-        -- Extend block navigation to language keywords
-        use 'andymass/vim-matchup'
+		-- Argument Utility
+		use("AndrewRadev/sideways.vim")
 
-        -- Argument Utility
-        use 'AndrewRadev/sideways.vim'
+		-- Projects
+		use({
+			"ahmedkhalf/project.nvim",
+			config = function()
+				require("project_nvim").setup({})
+			end,
+		})
 
-        -- Projects
-        use {
-            "ahmedkhalf/project.nvim",
-            config = function() require("project_nvim").setup() end,
-        }
+		-- Marks
+		use({
+			"ThePrimeagen/harpoon",
+			requires = { "nvim-lua/plenary.nvim", "nvim-lua/popup.nvim" },
+		})
 
-        -- Marks
-        use {
-            "ThePrimeagen/harpoon",
-            requires = {"nvim-lua/plenary.nvim", "nvim-lua/popup.nvim"},
-        }
+		-- Create parent folders when writing new file
+		use("jessarcher/vim-heritage")
 
-        use {
-            "antoinemadec/FixCursorHold.nvim",
-            run = function() vim.g.curshold_updatime = 1000 end,
-        }
-    end,
+		-- Editor Config per project
+		use("editorconfig/editorconfig-vim")
 
-    config = {
-        display = {
-            open_fn = function()
-                return require('packer.util').float({border = 'single'})
-            end,
-        },
-    },
+		-- Async compilation
+		use("tpope/vim-dispatch")
+
+		-- Testing
+		use("vim-test/vim-test")
+
+		use({
+			"antoinemadec/FixCursorHold.nvim",
+			run = function()
+				vim.g.curshold_updatime = 1000
+			end,
+		})
+
+		use("ThePrimeagen/vim-be-good")
+	end,
+
+	config = {
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "single" })
+			end,
+		},
+	},
 })
