005408e8084772803404c2bcf8a28c169931f2b7
Derek Vasconcelos <derek@cepetro.unicamp.br>
2021-12-08 20:17:08 -0300

    LSP updates

--- nvim/.config/nvim/lua/dbvasconcelos/lsp/init.lua
+++ nvim/.config/nvim/lua/dbvasconcelos/lsp/init.lua
@@ -8,12 +8,12 @@ require("dbvasconcelos.lsp.pictograms").setup()
 require("dbvasconcelos.lsp.handlers").setup()
 
 -- Comments
-require("nvim_comment").setup({create_mappings = false})
+require("Comment").setup()
 
 -- On init
 local on_init = function(client)
-    client.config.flags = client.config.flags or {}
-    client.config.flags.allow_incremental_sync = true
+	client.config.flags = client.config.flags or {}
+	client.config.flags.allow_incremental_sync = true
 end
 
 local lsp_mappings = require("dbvasconcelos.lsp.keybindings")
@@ -21,90 +21,96 @@ local servers = require("dbvasconcelos.lsp.servers").servers
 
 -- On Attach
 local on_attach = function(client)
-    -- Mappings
-    lsp_mappings.on_attach(client)
+	-- Mappings
+	lsp_mappings.on_attach(client)
 
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+	-- Enable completion triggered by <c-x><c-o>
+	vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- Highlighting
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec([[
+	-- Highlighting
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
             augroup lsp_document_highlight
             autocmd! * <buffer>
             autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
             autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
             augroup END
-            ]], false)
-    end
+            ]],
+			false
+		)
+	end
 
-    -- Code Lens
-    if client.resolved_capabilities.code_lens then
-        vim.api.nvim_exec([[
+	-- Code Lens
+	if client.resolved_capabilities.code_lens then
+		vim.api.nvim_exec(
+			[[
             augroup lsp_document_codelens
             au! * <buffer>
             autocmd BufWritePost,CursorHold <buffer> lua vim.lsp.codelens.refresh()
             augroup END
-            ]], false)
-    end
+            ]],
+			false
+		)
+	end
 
-    -- Attach any filetype specific options to the client
-    local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-    if servers[filetype] and servers[filetype].callback then
-        servers[filetype].callback(client)
-    end
+	-- Attach any filetype specific options to the client
+	local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+	if servers[filetype] and servers[filetype].callback then
+		servers[filetype].callback(client)
+	end
 end
 
 -- Extended capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities =
---     vim.tbl_deep_extend("keep", capabilities, lsp_status.capabilities)
-capabilities.textDocument.codeLens = {dynamicRegistration = false}
+capabilities.textDocument.codeLens = { dynamicRegistration = false }
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport =
-    {properties = {"documentation", "detail", "additionalTextEdits"}}
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = { "documentation", "detail", "additionalTextEdits" },
+}
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 -- Setup
-local lspconfig = require("lspconfig")
 local setup_server = function(server, config)
-    if not config then return end
+	if not config then
+		return
+	end
 
-    if type(config) ~= "table" then config = {} end
+	if type(config) ~= "table" then
+		config = {}
+	end
 
-    config = vim.tbl_deep_extend("force", {
-        on_init = on_init,
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {debounce_text_changes = 50},
-    }, config)
+	config = vim.tbl_deep_extend("force", {
+		on_init = on_init,
+		on_attach = on_attach,
+		capabilities = capabilities,
+		flags = { debounce_text_changes = 50 },
+	}, config)
 
-    lspconfig[server].setup(config)
+	server:setup(config)
 end
 
 -- Install
-local lspinstall = require('lspinstall')
-local install_servers = function()
-    lspinstall.setup()
+local lsp_installer = require("nvim-lsp-installer")
 
-    local formatters = {}
-    for filetype, config in pairs(servers) do
-        if config.formatter then
-            formatters[filetype] = {function()
-                return config.formatter
-            end}
-        end
-        setup_server(config.server, config.lsp)
-    end
-
-    -- Formatters
-    require('formatter').setup({logging = false, filetype = formatters})
+local formatters = {}
+for filetype, config in pairs(servers) do
+	local supported, server = lsp_installer.get_server(config.server)
+	if supported then
+		if not server:is_installed() then
+			print("Installing " .. config.server)
+			server:install()
+		end
+	end
+	if config.formatter then
+		formatters[filetype] = {
+			function()
+				return config.formatter
+			end,
+		}
+	end
+	setup_server(server, config.lsp)
 end
 
-install_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-lspinstall.post_install_hook = function()
-    install_servers()
-    vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
-end
+-- Formatters
+require("formatter").setup({ logging = false, filetype = formatters })
--- nvim/.config/nvim/lua/dbvasconcelos/lsp/servers.lua
+++ nvim/.config/nvim/lua/dbvasconcelos/lsp/servers.lua
@@ -1,74 +1,77 @@
 local M = {}
 
 M.servers = {
-    go = {
-        server = "go",
-        lsp = {
-            settings = {gopls = {codelenses = {test = true}}},
-            flags = {debounce_text_changes = 200},
-        },
-    },
+	go = {
+		server = "gopls",
+		lsp = {
+			settings = { gopls = { codelenses = { test = true } } },
+			flags = { debounce_text_changes = 200 },
+		},
+	},
 
-    cpp = {
-        server = "clangd",
-        lsp = {
-            cmd = {
-                "clangd", "--background-index", "--suggest-missing-includes",
-                "--clang-tidy", "--header-insertion=iwyu",
-            },
-        },
-        formatter = {
-            exe = "clang-format",
-            args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
-            stdin = true,
-            cwd = vim.fn.expand('%:p:h'), -- Run clang-format in cwd of the file.
-        },
-    },
+	cpp = {
+		server = "clangd",
+		lsp = {
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--suggest-missing-includes",
+				"--clang-tidy",
+				"--header-insertion=iwyu",
+			},
+		},
+		formatter = {
+			exe = "clang-format",
+			args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+			stdin = true,
+			cwd = vim.fn.expand("%:p:h"), -- Run clang-format in cwd of the file.
+		},
+	},
 
-    lua = {
-        server = "lua",
-        lsp = {
-            settings = {
-                Lua = {
-                    runtime = {
-                        -- LuaJIT in the case of Neovim
-                        version = 'LuaJIT',
-                        path = vim.split(package.path, ';'),
-                    },
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = {'vim'},
-                    },
-                    workspace = {
-                        -- Make the server aware of Neovim runtime files
-                        library = {
-                            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                        },
-                    },
-                },
-            },
-        },
-        formatter = {
-            exe = "lua-format",
-            args = {"--extra-sep-at-table-end"},
-            stdin = true,
-        },
-    },
+	lua = {
+		server = "sumneko_lua",
+		lsp = {
+			settings = {
+				Lua = {
+					runtime = {
+						-- LuaJIT in the case of Neovim
+						version = "LuaJIT",
+						path = vim.split(package.path, ";"),
+					},
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+						},
+					},
+				},
+			},
+		},
+		formatter = {
+			exe = "stylua",
+			args = { "--column-width 100" },
+			stdin = false,
+		},
+	},
 
-    php = {server = "php", lsp = true},
+	php = { server = "intelephense", lsp = true },
 
-    vim = {server = "vim", lsp = true},
+	vim = { server = "vimls", lsp = true },
 
-    bash = {server = "bash", lsp = true},
+	bash = { server = "bashls", lsp = true },
 
-    sh = {server = "bash", lsp = true},
+	sh = { server = "bashls", lsp = true },
 
-    html = {server = "html", lsp = true},
+	html = { server = "html", lsp = true },
 
-    json = {server = "json", lsp = true},
+	json = { server = "jsonls", lsp = true },
 
-    dockerfile = {server = "dockerfile", lsp = true},
+	dockerfile = { server = "dockerls", lsp = true },
 }
 
 return M
