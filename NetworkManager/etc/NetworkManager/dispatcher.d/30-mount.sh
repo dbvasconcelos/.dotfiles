8544586d6fa52cd4f1f7864966d6f23035c36e86
Derek Vasconcelos <derek@cepetro.unicamp.br>
2024-11-05 11:47:59 -0300

    updated mount script

--- NetworkManager/etc/NetworkManager/dispatcher.d/30-mount.sh
+++ NetworkManager/etc/NetworkManager/dispatcher.d/30-mount.sh
@@ -4,20 +4,20 @@
 event="$2"
 
 if [ "$CONNECTION_ID" = "tun0" ]; then
-    mnt_parent="/mnt/UNISIM"
+    mnt="/mnt/UNISIM"
     case "$event" in
     "up")
-        smb_srv="//ls2.unisim.cepetro.unicamp.br/scratch/derek"
-        smb_opts="uid=derek,credentials=/home/derek/.config/UNISIM/credentials"
-        mount -t cifs -m -o "$smb_opts" "$smb_srv" "$mnt_parent/scratch"
+        smb_srv="//ls2.unisim.cepetro.unicamp.br/scratch/$USER"
+        smb_opts="uid=$USER,credentials=$XDG_CONFIG_HOME/UNISIM/credentials"
+        mount -t cifs -m -o "$smb_opts" "$smb_srv" "$mnt/scratch"
 
         nfs_srv="fls.unisim.cepetro.unicamp.br:/mnt/data"
-        mount -t nfs -m "$nfs_srv/home/UNISIM/derek" "$mnt_parent/home"
-        mount -t nfs -m "$nfs_srv/TEC" "$mnt_parent/tec"
-        mount -t nfs -m "$nfs_srv/transfer" "$mnt_parent/transfer"
+        mount -t nfs -m "$nfs_srv/home/UNISIM/derek" "$mnt/home"
+        mount -t nfs -m "$nfs_srv/TEC" "$mnt/tec"
+        mount -t nfs -m "$nfs_srv/transfer" "$mnt/transfer"
         ;;
     "down" | "pre-down" | "vpn-pre-down")
-        umount -f "$mnt_parent"/* | true
+        umount -f "$mnt"/* | true
         ;;
     esac
 fi
