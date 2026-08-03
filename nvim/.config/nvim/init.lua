50e530de999900370fa3651d43ff0d18aa3692bc
Derek Vasconcelos <derekvasconcelos@gmail.com>
2026-08-03 08:30:25 -0300

    nvim sent to own repo

--- nvim/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("config.globals")
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("config.extensions")
-require("config.lsp")
-require("config.lazy")
--- nvim/.config/nvim/lua/plugins/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ "brenoprata10/nvim-highlight-colors", opts = {} },
-	{ "j-hui/fidget.nvim", opts = {} },
-	{ "kylechui/nvim-surround", opts = {} },
-	{ "numToStr/Comment.nvim", opts = {} },
-	{ "tpope/vim-sleuth" },
-}
